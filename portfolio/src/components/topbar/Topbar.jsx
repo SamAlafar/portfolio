5008fcb26619cd8c81bbdd1c4ac10ab5c8c8d4f4
Sam Alafar <sam.alafar@gmail.com>
2022-03-02 11:26:30 +0100

    created onClick function wheb clicking on hamburger menu and

--- portfolio/src/components/topbar/Topbar.jsx
+++ portfolio/src/components/topbar/Topbar.jsx
@@ -1,14 +1,32 @@
 import './topbar.scss';
+import { Person, Mail } from '@material-ui/icons';
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className='topbar'>
-        <div className="wrapper">
-            <div className="left">
-                <a href="#intro"></a>
-            </div>
-            <div className="right"></div>
+    <div className={'topbar ' + (menuOpen && 'active')}>
+      <div className='wrapper'>
+        <div className='left'>
+          <a href='#intro' className='logo'>
+            {' '}
+            Sam Alafar
+          </a>
+          <div className='itemContainer'>
+            <Person className='icon' />
+            <span>+34 645 674 646</span>
+          </div>
+          <div className='itemContainer'>
+            <Mail className='icon' />
+            <span>samalafar@gmail.com</span>
+          </div>
         </div>
+        <div className='right'>
+          <div className='hamburger' onClick={() => setMenuOpen(!menuOpen)}>
+            <span className='line1'></span>
+            <span className='line2'></span>
+            <span className='line3'></span>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
