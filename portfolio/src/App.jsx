e4709dfc8c187bb42fb8464f19e82479d65a2eed
Sam Alafar <sam.alafar@gmail.com>
2022-03-02 09:47:53 +0100

    imported different components

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,8 +1,19 @@
 import Topbar from "./components/topbar/Topbar";
+import Intro from "./components/intro/Intro";
+import Works from "./components/works/Works";
+import Portfolio from "./components/portfolio/Portfolio";
+import Contact from "./components/contact/Contact";
+import './app.scss';
 
 function App() {
   return <div className='app'>
     <Topbar/>
+    < div className='sections'>
+      <Intro/>
+      <Works/>
+      <Portfolio/>
+      <Contact/>
+    </div>
   </div>;
 }
 
