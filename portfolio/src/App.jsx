8161cd36b3e534fd859f438f68fd7a784ed7debe
Sam Alafar <sam.alafar@gmail.com>
2022-03-02 11:24:06 +0100

    imported useState and passed them to the topbar component

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,20 +1,24 @@
-import Topbar from "./components/topbar/Topbar";
-import Intro from "./components/intro/Intro";
-import Works from "./components/works/Works";
-import Portfolio from "./components/portfolio/Portfolio";
-import Contact from "./components/contact/Contact";
+import Topbar from './components/topbar/Topbar';
+import Intro from './components/intro/Intro';
+import Works from './components/works/Works';
+import Portfolio from './components/portfolio/Portfolio';
+import Contact from './components/contact/Contact';
 import './app.scss';
+import { useState } from 'react';
 
 function App() {
-  return <div className='app'>
-    <Topbar/>
-    < div className='sections'>
-      <Intro/>
-      <Works/>
-      <Portfolio/>
-      <Contact/>
+  const [menuOpen, setMenuOpen] = useState(false);
+  return (
+    <div className='app'>
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <div className='sections'>
+        <Intro />
+        <Works />
+        <Portfolio />
+        <Contact />
+      </div>
     </div>
-  </div>;
+  );
 }
 
 export default App;
